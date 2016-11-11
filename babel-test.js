dfc2bcd3e84b4f2e6aeb387c4bf2e2823e1adec6
davidrauch <mail@davidrauch.at>
2016-11-11 11:35:18 +0100

    SQUASHED: AUTO-COMMIT-babel-test.js,

--- babel-test.js
+++ babel-test.js
@@ -1,4 +1,20 @@
+//System.paths['*'] = '../lively4-projectional-editor/node_modules/';
+//console.log("ASDF");
+//console.log(System.paths);
 import * as babylon from '../lively4-projectional-editor/node_modules/babylon/lib/index.js';
+//import * as types from "../lively4-projectional-editor/node_modules/babel-types/lib/index.js";
+
+/*oldBaseURL = System.baseURL;
+System.baseURL = 'https://lively-kernel.org/lively4/lively4-projectional-editor/';
+import * as babylon from 'babylon/lib/index.js';
+System.baseURL = oldBaseURL;
+*/
+//delete System.paths['*'];
+//import * as generator from '../lively4-projectional-editor/node_modules/babel-generator/lib/index.js';
+//import generate from "babel-generator";
+//import traverse from "babel-traverse";
+//import * as t from "babel-types";
+//import * as bt from "./blockly-tools";
 
 const transformCodeOnClick = function(source_id, button_id, target_id) {
   $('#' + button_id).on('click', function() {
