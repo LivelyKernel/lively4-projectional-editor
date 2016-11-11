afe5a63ec48fa8961ab27805ee309ea421177a43
davidrauch <mail@davidrauch.at>
2016-11-11 10:24:23 +0100

    SQUASHED: SQUASHED-AUTO-COMMIT-babel-test.js-AUTO-COMMIT-babel.html-AUTO-COMMIT-editor.html,

--- babel-test.js
+++ babel-test.js
@@ -1,13 +1,13 @@
-System.import('../lively4-projectional-editor/node_modules/babylon/lib/index.js').then(
-  function(babylon) {
-    
-    $('#ast-transform-button').on('click', function() {
-      const code_source = $('#code-source');
-      const target = $('#ast-target');
+import * as babylon from '../lively4-projectional-editor/node_modules/babylon/lib/index.js';
+
+const transformCodeOnClick = function(source_id, button_id, target_id) {
+  $('#' + button_id).on('click', function() {
+      const code_source = $('#' + source_id);
+      const target = $('#' + target_id);
       
       const ast = babylon.parse(code_source.val());
       target.text(JSON.stringify(ast.program, undefined, 4));
     });
-    
-  }
-);
+}
+
+export { transformCodeOnClick };
