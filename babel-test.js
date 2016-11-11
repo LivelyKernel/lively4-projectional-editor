e0ea8f4ea7c3cdfcab7e9ef0fb404fda40e70fdc
davidrauch <mail@davidrauch.at>
2016-11-11 16:23:11 +0100

    SQUASHED: AUTO-COMMIT-babel-test.js,AUTO-COMMIT-babel.html,

--- babel-test.js
+++ babel-test.js
@@ -1,31 +1,10 @@
-//System.paths['*'] = '../lively4-projectional-editor/node_modules/';
-//console.log("ASDF");
-//console.log(System.paths);
-//import * as babylon from '../lively4-projectional-editor/node_modules/babylon/lib/index.js';
-//import * as types from "../lively4-projectional-editor/node_modules/babel-types/lib/index.js";
-
-/*oldBaseURL = System.baseURL;
-System.baseURL = 'https://lively-kernel.org/lively4/lively4-projectional-editor/';
-import * as babylon from 'babylon/lib/index.js';
-System.baseURL = oldBaseURL;
-*/
-//delete System.paths['*'];
-//import * as generator from '../lively4-projectional-editor/node_modules/babel-generator/lib/index.js';
-//import generate from "babel-generator";
-//import traverse from "babel-traverse";
-//import * as t from "babel-types";
-//import * as bt from "./blockly-tools";
-
-import * as deps from '../lively4-projectional-editor/dependencies.js';
-console.log("DEPS");
-console.log(deps);
 
 const transformCodeOnClick = function(source_id, button_id, target_id) {
   $('#' + button_id).on('click', function() {
       const code_source = $('#' + source_id);
       const target = $('#' + target_id);
       
-      const ast = babylon.parse(code_source.val());
+      const ast = lpe.babylon.parse(code_source.val());
       target.text(JSON.stringify(ast.program, undefined, 4));
     });
 }
