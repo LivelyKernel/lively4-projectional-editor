e0a2d7f9e44daab9a8e41866cb640c0e06efa9ee
David Rauch <mail@davidrauch.at>
2016-11-11 16:03:17 +0100

    wip

--- dependencies.js
+++ dependencies.js
@@ -1,18 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.babylon = undefined;
-
 var _babylon = require("babylon");
 
 var babylon = _interopRequireWildcard(_babylon);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-exports.babylon = babylon;
+window.lpe = {};
+window.lpe.babylon = babylon;
 },{"babylon":2}],2:[function(require,module,exports){
 "use strict";
 
