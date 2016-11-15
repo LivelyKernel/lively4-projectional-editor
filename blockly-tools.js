8b0d2e78c1e88bba6351fa3c9344a272ca03e1d6
davidrauch <mail@davidrauch.at>
2016-11-15 16:50:01 +0100

    Fixed blockly loading
    SQUASHED: Fixed-blockly-loading,

--- blockly-tools.js
+++ blockly-tools.js
@@ -1,6 +1,5 @@
 // sets block as first statment for input 'inputName' of block 'intoBlock'
 export function setAsFirstStatement(block, intoBlock, inputName) {
-	let Blockly = lpe_lib.blockly;
 	let localConnection = new Blockly.RenderedConnection(intoBlock, Blockly.NEXT_STATEMENT);
 	let otherConnection = new Blockly.RenderedConnection(block, Blockly.PREVIOUS_STATEMENT);
 	
@@ -14,7 +13,6 @@ export function setAsFirstStatement(block, intoBlock, inputName) {
 
 // sets block as as input 'inputName' of block 'intoBlock'
 export function setAsInput(block, intoBlock, inputName) {
-	let Blockly = lpe_lib.blockly;
 	let localConnection = new Blockly.RenderedConnection(intoBlock, Blockly.INPUT_VALUE);
 	let otherConnection = new Blockly.RenderedConnection(block, Blockly.OUTPUT_VALUE);
 	
@@ -28,7 +26,6 @@ export function setAsInput(block, intoBlock, inputName) {
 
 // sets block as as next of intoBlock
 export function setAsNext(block, intoBlock) {
-	let Blockly = lpe_lib.blockly;
 	let localConnection = new Blockly.RenderedConnection(intoBlock, Blockly.NEXT_STATEMENT);
 	let otherConnection = new Blockly.RenderedConnection(block, Blockly.PREVIOUS_STATEMENT);
 	
