8c3437298ab4c9048323f282eebc7acf85a1be58
davidrauch <mail@davidrauch.at>
2016-11-23 19:11:59 +0100

    Added basic reverse editing
    SQUASHED: SQUASHED-AUTO-COMMIT-lib-babel_tools.js-AUTO-COMMIT-templates-lively-projectional-editor.js,

--- templates/lively-projectional-editor.js
+++ templates/lively-projectional-editor.js
@@ -33,9 +33,17 @@ export default class ProjectionalEditor extends Morph {
   
   // Binds the content of the text- and block views
   bindViews() {
+    // Initialize mute variable
+    this.muteTextEditor = false;
+    this.muteBlockEditor = false;
+    
     // Update block view when the text view changes
     let timeout;
     this.textEditor.addEventListener('change', (evt) => {
+      if(this.muteTextEditor) {
+        return;
+      }
+      
       // Cancel the old timeout
       if(timeout) {
         clearTimeout(timeout);
@@ -43,9 +51,25 @@ export default class ProjectionalEditor extends Morph {
       
       // Start a new timeout
       timeout = setTimeout(() => {
+        // Mute the change events from the block view
+        this.muteBlockEditor = true;
+        setTimeout(() => {
+          this.muteBlockEditor = false;
+        }, 2000);
+        
+        
         // If the content hasn't changed for 1 second, update the block view
-        this.blockWorkspace.clear();
-        babel_tools.createBlocksForCode(this.textEditor.value, this.blockWorkspace);
+        try {
+      	  this.ast = lpe_babel.babylon.parse(this.textEditor.value);
+      	  this.blockWorkspace.clear();
+      	  babel_tools.createBlocksForAST(this.ast, this.blockWorkspace);
+      	  console.log(this.ast);
+        } catch (e) {
+          console.error("LPE: Could not parse code");
+          this.blockWorkspace.clear();
+        }
+        
+        //babel_tools.createBlocksForCode(this.textEditor.value, this.blockWorkspace);
       }, 1000);
     })
   }
@@ -80,14 +104,85 @@ export default class ProjectionalEditor extends Morph {
     }, 2000);
     
     // Listen for changes on the Blockly workspace
+    var lastClickedBlock = '';
+    var lastClickedTime = (new Date()).getTime();
     this.blockWorkspace.addChangeListener((event) => {
       if(event.type === Blockly.Events.MOVE) {
         let block = this.blockWorkspace.getBlockById(event.blockId);
         if(block) {
           blockly_tools.updateBlockConnections(block);
         }
+      } else if(event.type === Blockly.Events.UI && event.element === 'click') {
+        const newTime = (new Date()).getTime();
+        if(lastClickedBlock === event.blockId && (newTime - lastClickedTime) < 1000) {
+          const clickedBlock = this.blockWorkspace.getBlockById(event.blockId);
+          clickedBlock.setCollapsed(!clickedBlock.isCollapsed());
+          
+          lastClickedBlock = '';
+          lastClickedTime = 0;
+        } else {
+          lastClickedBlock = event.blockId;
+          lastClickedTime = newTime;
+        }
+      } else if(event.type === Blockly.Events.UI && event.element === 'selected') {
+        if(!event.newValue) {
+          return;
+        }
+        
+        let block = this.blockWorkspace.getBlockById(event.newValue);
+        let node = block.babel_node;
+        
+        if(typeof(node.start) !== 'undefined' && typeof(node.end) !== 'undefined') {
+          // Select the corresponding text in the text editor
+          this.selectTextRange(this.textEditor.value, node.start, node.end);
+        }
+      } else if(event.type === Blockly.Events.CHANGE) {
+        if(!this.muteBlockEditor) {
+          let block = this.blockWorkspace.getBlockById(event.blockId);
+          let node = block.babel_node;
+          
+          // Change the value in the part of the AST that belongs to this block
+          node[event.name] = event.newValue;
+          
+          try {
+            // Generate AST
+            let generated = lpe_babel.generate(this.ast);
+
+            // Set value in text editor
+            this.muteTextEditor = false;
+            this.textEditor.value = generated.code;
+            
+            // Select the new value in the text editor
+            this.selectTextRange(generated.code, node.start, node.end);
+          } catch (e) {
+            console.error("LPE: Could not generate code");
+          }
+        }
       }
     });
+  }
+  
+  // Turns an absolute position in a text into a row and column position
+  getLineAndColumnForPosition(text, pos) {
+    let line = 0;
+    let col = 0;
+    for(let i = 0; i < pos; i++) {
+      col++;
+      if(text[i] == '\n') {
+        line++;
+        col = 0;
+      }
+    }
+    
+    return [line, col];
+  }
+  
+  // Selects a range of text in the text editor
+  selectTextRange(text, start, end) {
+    const Range = ace.require("ace/range").Range;
+    const rangeStart = this.getLineAndColumnForPosition(text, start);
+    const rangeEnd = this.getLineAndColumnForPosition(text, end);
+    this.textEditor.editor.getSelection().setRange(new Range(rangeStart[0], rangeStart[1], rangeEnd[0], rangeEnd[1]));
   }
 
   // Utility function to get a part of the component
