caed282f8665aacb5fabd667ae095f44caacdc57
David Rauch <mail@davidrauch.at>
2017-01-10 19:44:01 +0100

    Added example for renaming

--- examples.js
+++ examples.js
@@ -1,9 +1,31 @@
 var a = 1, b = 2;
-if(a==b) {
-	console.log("equal");
+if(a > b) {
+	console.log("a is larger!");
 }
+
+
 
 
 var a = 1;
 var b = 2;
 var c = 3;
+
+
+
+
+const a = [1, 2, 3];
+
+a.forEach(function(e) {
+	console.log(e);
+})
+
+
+
+function f1(a) {
+	console.log(a);
+}
+
+function f2() {
+	var a = "test";
+	console.log(a);
+}
